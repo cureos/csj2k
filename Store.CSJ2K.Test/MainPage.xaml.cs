08fb78544b9e3d76eeacaaf4093cef60aa16a461
Anders Gustafsson <anders@cureos.com>
2015-04-16 20:12:50 +0200

    Allow multiple stream encoding

--- Store.CSJ2K.Test/MainPage.xaml.cs
+++ Store.CSJ2K.Test/MainPage.xaml.cs
@@ -1,23 +1,27 @@
-﻿using System;
-using System.IO;
-using CSJ2K;
-using Windows.Storage;
-using Windows.Storage.Pickers;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Store.CSJ2K.Test
 {
+	using System;
+	using System.IO;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	using Windows.Storage;
+	using Windows.Storage.Pickers;
+	using Windows.UI.Xaml;
 	using Windows.UI.Xaml.Media;
+	using Windows.UI.Xaml.Navigation;
+
+	using global::CSJ2K;
 
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
 	public sealed partial class MainPage
 	{
+		private FileOpenPicker picker;
+
 		public MainPage()
 		{
 			InitializeComponent();
@@ -34,39 +38,46 @@ namespace Store.CSJ2K.Test
 
 		private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
 		{
+			var j2KExts = new[] { ".jp2", ".j2k", ".j2c" };
+
 			try
 			{
-				var picker = new FileOpenPicker { SuggestedStartLocation = PickerLocationId.DocumentsLibrary };
-				picker.FileTypeFilter.Add(".jp2");
-				picker.FileTypeFilter.Add(".ppm");
-				picker.FileTypeFilter.Add(".pgm");
-				picker.FileTypeFilter.Add(".pgx");
+				var files = await picker.PickMultipleFilesAsync();
+				if (files == null) return;
 
-				var file = await picker.PickSingleFileAsync();
-				if (file == null) return;
+				var streams =
+					await Task.WhenAll(files.Select(async file => (await file.OpenAsync(FileAccessMode.Read)).AsStreamForRead()));
 
-				using (var stream = await file.OpenAsync(FileAccessMode.Read))
+				ImageSource image;
+				if (streams.Length == 1 && j2KExts.Any(ext => files[0].FileType.Equals(ext, StringComparison.OrdinalIgnoreCase)))
 				{
-					ImageSource image;
-					if (file.FileType.Equals(".jp2"))
-					{
-						image = (ImageSource)J2kImage.FromStream(stream.AsStreamForRead());
-					}
-					else
-					{
-						// If not already encoded, encode before decoding
-						var bytes = J2kImage.ToBytes(stream.AsStreamForRead());
-						image = (ImageSource)J2kImage.FromBytes(bytes);
-					}
-					DecodedImage.Source = image;
-					ImageName.Text = file.Path;
+					image = (ImageSource)J2kImage.FromStream(streams[0]);
 				}
+				else
+				{
+					// If not already encoded, encode before decoding
+					var bytes = J2kImage.ToBytes(streams);
+					image = (ImageSource)J2kImage.FromBytes(bytes);
+				}
+				DecodedImage.Source = image;
+				ImageName.Text = files[0].Path;
 			}
 			catch (Exception exc)
 			{
 				DecodedImage.Source = null;
 				ImageName.Text = "Could not display file, reason: " + exc.Message;
 			}
+		}
+
+		private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			picker = new FileOpenPicker { SuggestedStartLocation = PickerLocationId.DocumentsLibrary };
+			picker.FileTypeFilter.Add(".jp2");
+			picker.FileTypeFilter.Add(".j2k");
+			picker.FileTypeFilter.Add(".j2c");
+			picker.FileTypeFilter.Add(".ppm");
+			picker.FileTypeFilter.Add(".pgm");
+			picker.FileTypeFilter.Add(".pgx");
 		}
 	}
 }
