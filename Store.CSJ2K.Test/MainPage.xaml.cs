3c9421181131cd3a919f0747a65af7c24b65e4f0
Anders Gustafsson <anders@cureos.com>
2015-04-16 17:35:12 +0200

    Added encoding functionality to Store example

--- Store.CSJ2K.Test/MainPage.xaml.cs
+++ Store.CSJ2K.Test/MainPage.xaml.cs
@@ -11,38 +11,62 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Store.CSJ2K.Test
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
+	using Windows.UI.Xaml.Media;
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.  The Parameter
-        /// property is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-        }
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
 
-	    private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-	    {
-		    var picker = new FileOpenPicker { SuggestedStartLocation = PickerLocationId.DocumentsLibrary };
-		    picker.FileTypeFilter.Add(".jp2");
-		    var file = await picker.PickSingleFileAsync();
-		    if (file == null) return;
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.  The Parameter
+		/// property is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+		}
 
-		    using (var stream = await file.OpenAsync(FileAccessMode.Read))
-		    {
-			    var image = (WriteableBitmap)J2kImage.FromStream(stream.AsStreamForRead());
-				DecodedImage.Source = image;
-			    ImageName.Text = file.Path;
-		    }
-	    }
-    }
+		private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				var picker = new FileOpenPicker { SuggestedStartLocation = PickerLocationId.DocumentsLibrary };
+				picker.FileTypeFilter.Add(".jp2");
+				picker.FileTypeFilter.Add(".ppm");
+				picker.FileTypeFilter.Add(".pgm");
+				picker.FileTypeFilter.Add(".pgx");
+
+				var file = await picker.PickSingleFileAsync();
+				if (file == null) return;
+
+				using (var stream = await file.OpenAsync(FileAccessMode.Read))
+				{
+					ImageSource image;
+					if (file.FileType.Equals(".jp2"))
+					{
+						image = (ImageSource)J2kImage.FromStream(stream.AsStreamForRead());
+					}
+					else
+					{
+						// If not already encoded, encode before decoding
+						var bytes = J2kImage.ToBytes(stream.AsStreamForRead());
+						image = (ImageSource)J2kImage.FromBytes(bytes);
+					}
+					DecodedImage.Source = image;
+					ImageName.Text = file.Path;
+				}
+			}
+			catch (Exception exc)
+			{
+				DecodedImage.Source = null;
+				ImageName.Text = "Could not display file, reason: " + exc.Message;
+			}
+		}
+	}
 }
