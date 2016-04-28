936c4e2d1324a543db74973152fa64c08fdaea9a
Anders Gustafsson <anders@cureos.com>
2016-04-28 09:07:21 +0200

    Added iOS platform assembly.

---
+++ CSJ2K.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2007-2016 CSJ2K contributors.
+// Licensed under the BSD 3-Clause License.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSJ2K.Platform")]
+[assembly: AssemblyDescription("CSJ2K Support Library for Xamarin iOS")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e242ff7a-8560-40bb-9786-198f0d701c2a")]
---
+++ CSJ2K/Util/IOSImage.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2007-2016 CSJ2K contributors.
+// Licensed under the BSD 3-Clause License.
+
+namespace CSJ2K.Util
+{
+    using System.Runtime.InteropServices;
+
+    using CoreGraphics;
+    using CoreImage;
+
+    using UIKit;
+
+    internal class IOSImage : ImageBase<CGImage>
+    {
+        #region CONSTRUCTORS
+
+        internal IOSImage(int width, int height, int numberOfComponents)
+            : base(width, height, numberOfComponents)
+        {
+        }
+
+        #endregion
+
+        #region METHODS
+
+        public override T As<T>()
+        {
+            if (typeof(T) == typeof(UIImage))
+            {
+                return (T)(object)new UIImage((CGImage)this.GetImageObject());
+            }
+
+            if (typeof(T) == typeof(CIImage))
+            {
+                return (T)(object)new CIImage((CGImage)this.GetImageObject());
+            }
+
+            return (T)this.GetImageObject();
+        }
+
+        protected override object GetImageObject()
+        {
+            var handle = GCHandle.Alloc(this.Bytes, GCHandleType.Pinned);
+
+            using (
+                var context = new CGBitmapContext(
+                    handle.AddrOfPinnedObject(),
+                    this.Width,
+                    this.Height,
+                    8,
+                    this.NumberOfComponents * this.Width,
+                    CGColorSpace.CreateDeviceRGB(),
+                    CGImageAlphaInfo.PremultipliedLast))
+            {
+                var image = context.ToImage();
+                handle.Free();
+                return image;
+            }
+        }
+
+        #endregion
+    }
+}
