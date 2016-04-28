22b70ec086dca54f7601c1c444077dfe7efde364
Anders Gustafsson <anders@cureos.com>
2016-04-28 09:54:21 +0200

    Added test project stub for iOS

---
+++ CSJ2K.iOS.Test/Main.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2007-2016 CSJ2K contributors.
+// Licensed under the BSD 3-Clause License.
+
+using UIKit;
+
+namespace CSJ2K.iOS.Test
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
---
+++ CSJ2K.iOS.Test/ViewController.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2007-2016 CSJ2K contributors.
+// Licensed under the BSD 3-Clause License.
+
+using System;
+
+using UIKit;
+
+namespace CSJ2K.iOS.Test
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
