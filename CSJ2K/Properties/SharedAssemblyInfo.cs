0b22889630edd680524b6df70dd531cfbc0ef500
Anders Gustafsson <anders@cureos.com>
2016-06-30 08:45:35 +0200

    Updated .NET Core to RTM

--- CSJ2K/Properties/SharedAssemblyInfo.cs
+++ CSJ2K/Properties/SharedAssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.9.0")]
-[assembly: AssemblyFileVersion("1.9.0.6")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
